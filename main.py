e940a41bf1eb159157fd1232f617f881e534a514
AndrewN <domiantor@yandex.ru>
2019-03-29 22:18:50 +0300

    Scaling added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 
 import pygame
 
@@ -12,11 +13,21 @@ class MapWindow(object):
 
     def __init__(self, width, height):
         self.z = 10
+        self.w, self.h = width, height
         self.map = Map(COORDINATES, self.z)
         self.get_map()
         pygame.init()
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.flip()
+
+    def get_point(self, x, y):
+        parallel_multiplier = math.cos(float(COORDINATES[0]) * math.pi / 180)
+        degrees_per_pixel_x = 360 / math.pow(2, self.z + 8)
+        degrees_per_pixel_y = 360 / math.pow(2, self.z + 8) * parallel_multiplier
+        point_lat = float(COORDINATES[0]) - degrees_per_pixel_y * (y - self.h / 2)
+        point_lng = float(COORDINATES[1]) + degrees_per_pixel_x * (x - self.w / 2)
+
+        return point_lat, point_lng
 
     def update(self):
         for event in pygame.event.get():
@@ -38,9 +49,8 @@ class MapWindow(object):
                         self.get_map()
                     else:
                         self.z = 2
-                print(self.z)
             if event.type == pygame.MOUSEMOTION:
-                print([float(coord) for coord in event.pos])
+                point = self.get_point(event.pos[0], event.pos[1])
 
     def draw(self):
         self.screen.blit(pygame.image.load(os.path.join('map_parts/', self.map.name)), (0, 0))
